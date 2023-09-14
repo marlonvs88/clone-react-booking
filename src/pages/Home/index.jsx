569579f644d6e6a8e08ac12913c4700ec6c20a38
Marlon Viana Souza <marlonvs.dev@gmail.com>
2023-09-14 20:10:41 -0300

    teste para deploy

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,14 +16,13 @@ const Home = () => {
       <Header />
       <C.homeContainer>
         <Featured />
-          <C.homeTitle1>Browse by property type</C.homeTitle1>
-          <PropertyList />
-          <C.homeTitle1>Homes guests love</C.homeTitle1>
-          <FeaturedProperties />
-          <MailList />
-          <Footer />
+        <C.homeTitle1>Browse by property type</C.homeTitle1>
+        <PropertyList />
+        <C.homeTitle1>Homes guests love.</C.homeTitle1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
       </C.homeContainer>
-
     </>
   )
 }
