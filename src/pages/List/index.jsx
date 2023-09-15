2a1f0b360e3ca373860ca0c077b34f1dae3d34f8
Marlon Viana Souza <marlonvs.dev@gmail.com>
2023-09-15 11:28:33 -0300

    config deploy vercel

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -4,7 +4,10 @@ import Navbar from '../../components/Navbar'
 
 const List = () => {
   return (
-    <div><Navbar /><Header type="list"/></div>
+    <div>
+      <Navbar />
+      <Header type="list" />
+    </div>
   )
 }
 
